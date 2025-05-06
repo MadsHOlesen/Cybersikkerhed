651b67483b8c0248b6b4e559da318e33a7b9e341
Mads Olesen <madsolesen20@gmail.com>
2025-05-06 11:06:45 +0200

    Orden

--- js/scenarios.js
+++ js/scenarios.js
@@ -1,36 +1,27 @@
 'use strict';
 
-// BUTTON EKSEMPEL
-
-/*
-// MANUEL erklæring af variabler
-const c1 = document.querySelector('#c1'); // Gemmer elementet, der har id="c1". Datatype: Object
-const c2 = document.querySelector('#c2'); // Gemmer elementet, der har id="c2". Datatype: Object
-const c3 = document.querySelector('#c3'); // Gemmer elementet, der har id="c3". Datatype: Object
-*/
-
 // NODELIST / ARRAY
 const btns = document.querySelectorAll('.btn-group button');
 const boxes = document.querySelectorAll('.feedback');
-const choices = [];  // Array til id på knappen, der er blevet klikket
+const choices = [];  
 
-// FUNCTION
-const checkAnswer = (e) => { // Funktion med en parameter (e)
+// KNAPPPER
+const checkAnswer = (e) => { 
     boxes.forEach(box => {
         box.style.display = 'none';
     })
-    choices.push(e.target.id); // Gemmer id i choices arrayet
-    console.log(choices); //Logger choices arrayet
+    choices.push(e.target.id); 
+    console.log(choices); 
 
-    switch (e.target.id) { // Forholder sig til det klikkede elements id
+    switch (e.target.id) {
         case 'c1': 
-            document.querySelector('#c1-f').style.display = 'block'; // Viser elementet med id="c1-f" 
+            document.querySelector('#c1-f').style.display = 'block'; //Start
         break;
         case 'c2': 
-            document.querySelector('#c2-f').style.display = 'block';
+            document.querySelector('#c2-f').style.display = 'block'; //Start
         break;
         case 'c3':
-            document.querySelector('#c3-f').style.display = 'block'; // Viser elementet med id="c3-f"
+            document.querySelector('#c3-f').style.display = 'block'; //Start
         break;
         case 'c1-1':
             document.querySelector('#c1-1f').style.display = 'block'; //Phising
@@ -59,14 +50,15 @@ const checkAnswer = (e) => { // Funktion med en parameter (e)
         case 'c3-3':
             document.querySelector('#c3-3f').style.display = 'block'; //Tracking-Cookies
             break; 
-        default: console.log("Error"); // Viser error, hvis id ikke findes
+        default: console.log("Error"); 
         
        
 
     }
 }
 
-let zoomTexts = document.querySelectorAll(".svar-k, .svar-f"); // Zoom ind og ud
+// ZOOM
+let zoomTexts = document.querySelectorAll(".svar-k, .svar-f"); 
 
     setInterval(() => {
       zoomTexts.forEach((text) => {
@@ -74,21 +66,8 @@ let zoomTexts = document.querySelectorAll(".svar-k, .svar-f"); // Zoom ind og ud
       });
     }, 500);
 
-/*
-// MANUEL tilføjelse af event listeners
-c1.addEventListener('click', checkAnswer); // Kalder funktionen checkAnswer() ved klik på knappen med id="c1"
-c2.addEventListener('click', checkAnswer); // Kalder funktionen checkAnswer() ved klik på knappen med id="c2"
-c3.addEventListener('click', checkAnswer); // Kalder funktionen checkAnswer() ved klik på knappen med id="c3"
-*/
 
-// FOREACH ITERATION METHOD tilføjelse af event listeners
 btns.forEach(btn => {
     btn.addEventListener('click', checkAnswer);
 })
 
-/*
-// FOR LOOP - kan bruges i stedet for FOREACH ITERATION METHOD - tilføjelse af event listeners
-for(let i = 0; i < btns.length; i++ ) {
-    btns[i].addEventListener('click', checkAnswer);
-}
-*/
